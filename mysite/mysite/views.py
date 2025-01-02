933a14db0f0677e79f56f956c3015db764a133c9
Masi Ba <masumebasiry2.mb@gmail.com>
2025-01-02 21:51:20 +0330

    Added views and template

---
+++ mysite/mysite/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    context = {
+        "message" : "Hello Django Developers",
+    }        
+    return render(request, "mysite/index.html", context)
